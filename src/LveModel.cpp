3cf23b8d632fe39a61af3cb00f195f56311f260e
Nico Rivera <nicorivera2002@gmail.com>
2025-11-13 16:20:17 +0100

    Implement model loading from file and enhance vertex structure with normal and UV attributes

--- src/LveModel.cpp
+++ src/LveModel.cpp
@@ -1,10 +1,27 @@
 #include "LveModel.hpp"
 
+#include "LveUtils.hpp"
+
 #define TINYOBJLOADER_IMPLEMENTATION
 #include <TinyObjLoader/TinyObjLoader.h>
 
+// #define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
+
 #include <cassert>
 #include <cstring>
+#include <unordered_map>
+
+namespace std {
+    template <>
+    struct hash<lve::LveModel::Vertex> {
+        size_t operator()(const lve::LveModel::Vertex& vertex) const {
+            size_t seed = 0;
+            lve::hashCombine(seed, vertex.position, vertex.color, vertex.normal, vertex.uv);
+            return seed;
+        }
+    };
+}
 
 namespace lve {
     LveModel::LveModel(
@@ -23,6 +40,15 @@ namespace lve {
             vkDestroyBuffer(lve_device.device(), index_buffer, nullptr);
             vkFreeMemory(lve_device.device(), index_buffer_memory, nullptr);
         }
+    }
+    
+    std::unique_ptr<LveModel> LveModel::createModelFromFile(
+        LveDevice& device,
+        const std::string& filepath
+    ) {
+        Builder builder{};
+        builder.loadModel(filepath);
+        return std::make_unique<LveModel>(device, builder);
     }
     
     void LveModel::createVertexBuffers(const std::vector<Vertex>& vertices) {
@@ -132,5 +158,68 @@ namespace lve {
         attribute_descriptions[1].offset = offsetof(Vertex, color);
 
         return attribute_descriptions;
+    }
+
+    void LveModel::Builder::loadModel(
+        const std::string&filepath
+    ) {
+        tinyobj::attrib_t attrib;
+        std::vector<tinyobj::shape_t> shapes;
+        std::vector<tinyobj::material_t> materials;
+
+        std::string warn, err;
+        if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filepath.c_str()))
+            throw std::runtime_error(warn + err);
+
+        vertices.clear();
+        indices.clear();
+
+        std::unordered_map<Vertex, uint32_t> unique_verteces{};
+        for (const auto& shape: shapes) {
+            for (const auto& index: shape.mesh.indices) {
+                Vertex vertex{};
+                if (index.vertex_index >= 0) {
+                    vertex.position = {
+                        attrib.vertices[3*index.vertex_index + 0],
+                        attrib.vertices[3*index.vertex_index + 1],
+                        attrib.vertices[3*index.vertex_index + 2]
+                    };
+
+                    auto color_index = 3*index.vertex_index + 2;
+                    if (color_index < attrib.colors.size()) {
+                        vertex.color =  {
+                            attrib.colors[color_index - 2],
+                            attrib.colors[color_index - 1],
+                            attrib.colors[color_index - 0]
+                        };
+                    }
+                    else {
+                        // Default color
+                        vertex.color = {1.f, 1.f, 1.f};
+                    }
+                }
+
+                if (index.normal_index >= 0) {
+                    vertex.normal = {
+                        attrib.normals[3*index.normal_index + 0],
+                        attrib.normals[3*index.normal_index + 1],
+                        attrib.normals[3*index.normal_index + 2]
+                    };
+                }
+                
+                if (index.texcoord_index >= 0) {
+                    vertex.uv = {
+                        attrib.texcoords[2*index.texcoord_index + 0],
+                        attrib.texcoords[2*index.texcoord_index + 1]
+                    };
+                }
+
+                if (unique_verteces.count(vertex) == 0) {
+                    unique_verteces[vertex] = static_cast<uint32_t>(vertices.size());
+                    vertices.push_back(vertex);
+                }
+                indices.push_back(unique_verteces[vertex]);
+            }
+        }
     }
 }
